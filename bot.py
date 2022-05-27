697a3cc3f00d6726cf8a6e099011f36d68a33d9d
Ilya Fofanov <66513936+Nihaochingiz@users.noreply.github.com>
2022-05-27 14:41:49 +0300

    add array user_data need to add checking_function

--- bot.py
+++ bot.py
@@ -3,18 +3,21 @@ from ast import keyword
 import telebot
 from telebot import types
 import re
-from telebot.handler_backends import BaseMiddleware
+from telebot import apihelper
+bot = telebot.TeleBot("5320653955:AAFLZ9_nwCyR6IFQ6ZPsiNQQ45z4R6-LULI")
 
-bot = telebot.TeleBot("5320653955:AAFLZ9_nwCyR6IFQ6ZPsiNQQ45z4R6-LULI",use_class_middlewares=True)
 
 @bot.message_handler(commands=['start', 'домики'])
 def start(message):
+    global user_data
+    user_data = []
     mess = f'Здравствуйте! Хотите узнать подробнее о курсе "Домики" 🏘️?Кликните на кнопку ниже, уважаемый {message.from_user.last_name,message.chat.id }'
     bot.send_message(message.chat.id,mess,parse_mode='html')
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     itembtn1 = types.KeyboardButton('Узнать о курсе')
     markup.add(itembtn1)
     bot.send_message(message.chat.id, "Кликните на кнопку ниже", reply_markup=markup)
+
 @bot.message_handler()    
 def get_phone(message):
     lid_message = f'С вами свяжется мой помощник и расскажет все о курсе. Напишите ваш номер телефона'
@@ -23,8 +26,13 @@ def get_phone(message):
 def get_user_name(message):
     sent_get_user_name = bot.send_message(message.chat.id,'А теперь напишите ваше имя',parse_mode='html')
     bot.register_next_step_handler(sent_get_user_name , say_bye_user)
+    user_phone = message.text
+    
+    user_data.append(user_phone)
 def say_bye_user(message):
     bot.send_message(message.chat.id,'До свидания, с вами свяжется специалист',parse_mode='html')
+    user_data.append(message.text)
+    print(user_data)
     
 
 
