b0c3c9f739c416cfcef8200e37c4796dbd7ad259
Ilya Fofanov <66513936+Nihaochingiz@users.noreply.github.com>
2022-05-28 16:17:32 +0300

    rename functions

--- bot.py
+++ bot.py
@@ -1,43 +1,39 @@
 
-from ast import keyword
 import telebot
 from telebot import types
-import re
-from telebot import apihelper
 bot = telebot.TeleBot("5320653955:AAFLZ9_nwCyR6IFQ6ZPsiNQQ45z4R6-LULI")
-
-
 @bot.message_handler(commands=['start', 'домики'])
 def start(message):
     global user_data
     user_data = []
-    mess = f'Здравствуйте! Хотите узнать подробнее о курсе "Домики" 🏘️?Кликните на кнопку ниже, уважаемый {message.from_user.last_name,message.chat.id }'
+    mess = f'Здравствуйте! Хотите узнать подробнее о курсе "Домики" 🏘️?Кликните на кнопку ниже, уважаемый {message.from_user.last_name }'
     bot.send_message(message.chat.id,mess,parse_mode='html')
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     itembtn1 = types.KeyboardButton('Узнать о курсе')
     markup.add(itembtn1)
     bot.send_message(message.chat.id, "Кликните на кнопку ниже", reply_markup=markup)
-
 @bot.message_handler()    
-def get_phone(message):
+def sent_to_sales(message):
     lid_message = f'С вами свяжется мой помощник и расскажет все о курсе. Напишите ваш номер телефона'
     sent_get_message = bot.send_message(message.chat.id,lid_message,parse_mode='html')
-    bot.register_next_step_handler(sent_get_message , get_user_name)
-def get_user_name(message):
+    bot.register_next_step_handler(sent_get_message , get_user_phone)
+def get_user_phone(message):
     sent_get_user_name = bot.send_message(message.chat.id,'А теперь напишите ваше имя',parse_mode='html')
-    bot.register_next_step_handler(sent_get_user_name , say_bye_user)
+    bot.register_next_step_handler(sent_get_user_name , get_user_name)
     user_phone = message.text
-    
     user_data.append(user_phone)
-def say_bye_user(message):
+def get_user_name(message):
     bot.send_message(message.chat.id,'До свидания, с вами свяжется специалист',parse_mode='html')
-    user_data.append(message.text)
+    user_name = message.text
+    user_data.append(user_name)
     print(user_data)
-    
+
+
+
+
 
 
 
 
 
 bot.polling(none_stop=True)
-
